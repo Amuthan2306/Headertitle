24c34dc18923c9ac06c7da8210784838b6f6ccd7
amuthan <amuthan.s@colanonline.com>
2023-03-07 15:17:39 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -10,54 +10,57 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 const App = (props) => {
   const {
     text,
-    onPress
+    onPress,
+    backgroundColor,
+    borderColor,
+    arrow_size,
+    arrow_color,
+    text_color,
+    height,
+    width,
+    text_fontSize,
+    text_fontWeight
   } = props
   return (
-    <View style={styles.container}>
-      <View style={styles.header_View}>
+    <View style={{
+      height: height,
+      width:width ,
+      flexDirection: 'row',
+      backgroundColor: backgroundColor,
+      alignItems: 'center',
+      alignSelf: 'center',
+      alignContent: 'center',
+      borderBottomWidth: 2,
+      borderColor: borderColor
+    }}>
+      <View style={{
+        height: '50%',
+        width: '15%',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
         <TouchableOpacity onPress={onPress}>
-          <Icon name='arrow-left' size={30} color={'black'} />
+          <Icon name='arrow-left' size={arrow_size} color={arrow_color} />
         </TouchableOpacity>
 
       </View>
-      <View style={styles.header_view2}>
-        <Text style={styles.header_title}>{text}</Text>
+      <View style={{
+        height: '100%',
+        width: '85%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center'
+      }}>
+        <Text style={{
+          fontSize: text_fontSize,
+          fontWeight: text_fontWeight,
+          right: 25,
+          color: text_color
+        }}>{text}</Text>
       </View>
     </View>
   );
 }
-const styles = {
-  container: {
-    height: '8%',
-    width: '100%',
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    alignSelf: 'center',
-    alignContent: 'center',
-    borderBottomWidth: 2,
-    borderColor: '#F3F3F3'
-  },
-  header_View: {
-    height: '50%',
-    width: '15%',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  header_view2: {
-    height: '100%',
-    width: '85%',
-    // borderWidth: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center'
-  },
-  header_title: {
-    fontSize: 20,
-    fontWeight: '700',
-    right: 25,
-    color: 'black'
-  }
-}
+
 export default App;
